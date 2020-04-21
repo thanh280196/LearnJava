05c1e71ff2a58b5ba17d2f76393ef94b874b9b8f
thanh280196 <thanh.280196@gmail.com>
2020-04-21 15:12:45 +0700

    Exercise2

---
+++ UsingVariable.java
@@ -0,0 +1,10 @@
+class UsingVariable{
+   public static void main(String args[]){
+	   // Khai báo 2 biến var1,var2
+	    int var1,var2;
+	    var1 = 1996;
+	    var2 = var1/2;
+	   // In ra màn hình Console
+           System.out.println("Bien thu nhat la " + var1 +"\n Bien thu hai la " + var2);
+	}
+}
